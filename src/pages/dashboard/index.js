f8ba73c59c33c771c9af4dd2ffdc755fc0735c00
jamtechdev <jtplartisan@gmail.com>
2025-05-22 17:38:41 +0530

    chnages - rs

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -453,10 +453,9 @@ const Dashboard = () => {
             </div>
             <div className="dashboard-footer">
                 <div className="flex-between flex-wrap gap-16">
-                    <p className="text-gray-300 text-13 fw-normal"> &copy; Copyright Edmate 2024, All Right Reserverd</p>
+                    <p className="text-gray-300 text-13 fw-normal"> &copy; Copyright LMS 2025, All Right Reserverd</p>
                     <div className="flex-align flex-wrap gap-16">
                         <a href="#" className="text-gray-300 text-13 fw-normal hover-text-main-600 hover-text-decoration-underline">License</a>
-                        <a href="#" className="text-gray-300 text-13 fw-normal hover-text-main-600 hover-text-decoration-underline">More Themes</a>
                         <a href="#" className="text-gray-300 text-13 fw-normal hover-text-main-600 hover-text-decoration-underline">Documentation</a>
                         <a href="#" className="text-gray-300 text-13 fw-normal hover-text-main-600 hover-text-decoration-underline">Support</a>
                     </div>
