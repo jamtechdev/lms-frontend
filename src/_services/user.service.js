f995d00aca7cb40c81977901dbd318bcc72b7fc8
jamtechdev <jtplartisan@gmail.com>
2025-05-27 18:31:05 +0530

    Api student - RS

--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -2,9 +2,12 @@ import axiosInstance from "./axiosInstance";
 
 const userService = {
     login,
+    loginStudent,
 };
-
 async function login(data) {
     return await axiosInstance.post(`/api/v1/auth/login`, data);
+}
+async function loginStudent(data) {
+    return await axiosInstance.post(`/api/v1/auth/student-login`, data);
 }
 export default userService;
