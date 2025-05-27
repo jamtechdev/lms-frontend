bb581e975c25f32dbe7525ee92ed4dc7a4ae5b18
jamtechdev <jtplartisan@gmail.com>
2025-05-27 15:36:14 +0530

    route added for the questions - RS

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,24 @@
-import React, { Suspense, lazy } from "react";
+import React, { lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const Login = lazy(() => import("../pages/login/index"));
 const Signup = lazy(() => import("../pages/signup/index"));
 const ForgotPassword = lazy(() => import("../pages/forgotpassword/index"));
 const Dashboard = lazy(() => import("../pages/dashboard/index"));
+const Questions = lazy(() => import("../pages/questions/index"))
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-      <Suspense>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/forgotpassword" element={<ForgotPassword />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </Suspense>
+      {/* <Suspense> */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/questions" element={<Questions />} />
+      </Routes>
+      {/* </Suspense> */}
     </BrowserRouter>
   );
 };
