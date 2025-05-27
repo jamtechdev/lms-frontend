e8a93134db93ef5d2999982aafdf47e390b57464
jamtechdev <jtplartisan@gmail.com>
2025-05-27 17:38:45 +0530

    nav fixed - RS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
             <div className="flex-align gap-16">
                 <button type="button" className="toggle-btn d-xl-none d-flex text-26 text-gray-500"><i className="ph ph-list"></i></button>
                 <form action="#" className="w-350 d-sm-block d-none">
-                    <div className="position-relative">
+                    <div className="position-relative searchBox">
                         <button type="submit" className="input-icon text-xl d-flex text-gray-100 pointer-event-none"><i className="ph ph-magnifying-glass"></i></button>
                         <input type="text" className="form-control ps-40 h-40 border-transparent focus-border-main-600 bg-main-50 rounded-pill placeholder-15" placeholder="Search..." />
                     </div>
