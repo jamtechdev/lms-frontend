f32b61579aa153acb423a6448f8ac74e7f468de4
jamtechdev <jtplartisan@gmail.com>
2025-05-29 12:15:21 +0530

    get level fix services - RS

--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -11,7 +11,7 @@ async function login(data) {
 async function loginStudent(data) {
     return await axiosInstance.post(`/api/v1/auth/student-login`, data);
 }
-async function getLevel(education) {
-    return await axiosInstance.get(`/api/v1/questions/levels?education_type=${education}`);
+async function getLevel(data) {
+    return await axiosInstance.get(`/api/v1/questions/levels?education_type=${data}`);
 }
 export default userService;
