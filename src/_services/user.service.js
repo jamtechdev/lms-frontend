4960980873e643a2b54f3d84b55b9c574a5627bc
jamtechdev <jtplartisan@gmail.com>
2025-05-30 12:17:22 +0530

    Intregrated getSubject api - RS

--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -3,11 +3,15 @@ import axiosInstance from "./axiosInstance";
 const userService = {
     login,
     loginStudent,
+    getSubject,
 };
 async function login(data) {
     return await axiosInstance.post(`/api/v1/auth/login`, data);
 }
 async function loginStudent(data) {
     return await axiosInstance.post(`/api/v1/auth/student-login`, data);
+}
+async function getSubject(data) {
+    return await axiosInstance.get(`/api/v1/questions/subjects?level_id=${data}`)
 }
 export default userService;
